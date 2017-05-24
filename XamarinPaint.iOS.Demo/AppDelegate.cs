1702430ec8d39d53b5642b6e97c93acdd7cd1f5b
Christoph Lückler <christoph.lueckler@ilogs.com>
2017-05-24 10:49:37 +0200

    Update demo ViewController loading

--- XamarinPaint.iOS.Demo/AppDelegate.cs
+++ XamarinPaint.iOS.Demo/AppDelegate.cs
@@ -4,8 +4,18 @@ using UIKit;
 namespace XamarinPaint.iOS.Demo
 {
 	[Register ("AppDelegate")]
-	public class AppDelegate : UIApplicationDelegate {
+	public class AppDelegate : UIApplicationDelegate
+    {
 		public override UIWindow Window { get; set; }
-	}
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+            Window.RootViewController = new UINavigationController(new PaintViewController());
+            Window.MakeKeyAndVisible();
+
+            return true;
+        }
+    }
 }
 
